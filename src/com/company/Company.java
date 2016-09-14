fd628561f808c50a7eb631d14909a5c56648651c
e_voe_000 <e_voetbal_guy@hotmail.com>
2016-09-14 15:54:01 +0200

    added company class

---
+++ src/com/company/Company.java
@@ -0,0 +1,31 @@
+package com.company;
+
+/**
+ * Created by e_voe_000 on 9/14/2016.
+ */
+public class Company {
+
+    class Jaap extends Thread {
+
+        @Override
+        public void run() {
+
+        }
+    }
+
+    class SoftwareDeveloper extends Thread {
+
+        @Override
+        public void run() {
+
+        }
+    }
+
+    class User extends Thread {
+
+        @Override
+        public void run() {
+
+        }
+    }
+}
