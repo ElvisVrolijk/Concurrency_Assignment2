c2d4b1c8b1cea25fb54aedba0ff56a83ad9f7c7b
e_voe_000 <e_voetbal_guy@hotmail.com>
2016-09-15 20:39:36 +0200

    added semephore and constructers

--- src/com/company/Company.java
+++ src/com/company/Company.java
@@ -1,9 +1,37 @@
 package com.company;
 
+import java.util.concurrent.Semaphore;
+
 /**
  * Created by e_voe_000 on 9/14/2016.
  */
 public class Company {
+
+    private static final int NR_OF_SOFTWARE_DEVELOPER = 6;
+    private static final int NR_OF_USER = 10;
+    private Jaap jaap;
+    private SoftwareDeveloper[] softwareDevelopers;
+    private User[] users;
+    private Semaphore reportProblem, inviteUser, reportArrival, userConsultationInvitation, beginUserConsultation,
+            softwareConsultationInvitation, beginSoftwareConsultion;
+
+    public Company() {
+
+        softwareDevelopers = new SoftwareDeveloper[NR_OF_SOFTWARE_DEVELOPER];
+        users = new User[NR_OF_USER];
+
+        for (int i = 0; i < NR_OF_SOFTWARE_DEVELOPER; i++) {
+            softwareDevelopers[i] = new SoftwareDeveloper("Developer " + i, i);
+            softwareDevelopers[i].start();
+        }
+
+        for (int i = 0; i < NR_OF_USER; i++) {
+            users[i] = new User("User " + i, i);
+            users[i].start();
+        }
+        jaap = new Jaap();
+        jaap.start();
+    }
 
     class Jaap extends Thread {
 
@@ -14,6 +42,12 @@ public class Company {
     }
 
     class SoftwareDeveloper extends Thread {
+        private int softwareDeveloperId;
+
+        public SoftwareDeveloper(String name, int softwareDeveloperId) {
+            super(name);
+            this.softwareDeveloperId = softwareDeveloperId;
+        }
 
         @Override
         public void run() {
@@ -22,6 +56,12 @@ public class Company {
     }
 
     class User extends Thread {
+        private int userId;
+
+        public User(String name, int userId) {
+            super(name);
+            this.userId = userId;
+        }
 
         @Override
         public void run() {
